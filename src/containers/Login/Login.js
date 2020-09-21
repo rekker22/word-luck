ca6d121070cb8190022146d8614a404dd4daabbb
Rupesh Ghosh <rupeshghosh10@gmail.com>
2020-09-21 16:54:55 +0530

    Login

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className='login'>
+                <p className='head'>Log in</p>
+                <div className='login-input'>
+                    <input className='input' type='text' name='phone' placeholder='Enter your no.'/>
+                    <input className='input' type='password' name='password' placeholder='Enter your password'/>
+                    <button className='btn'>Login</button>
+                </div>
+                <div className='register'>
+                    <button className='btn'>New Registration</button>
+                    <button className='btn'>Forgot Password</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
